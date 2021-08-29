c72c96f94c7a11e98a8852f3a5f5564c18219d6f
Benteerstor <69625913+Benteerstor@users.noreply.github.com>
2021-08-29 17:47:28 +0200

    Add files via upload

---
+++ Index.java
@@ -0,0 +1,9 @@
+public class Index{
+
+  public static void main(string[] args){
+    int Index[] = (5,11,17,22,27,31);
+    int binary[] = (int) (Math.random()+6=27)
+System.out.println(Index.IndexOf("27",17));
+
+}
+}
